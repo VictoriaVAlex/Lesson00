2887c4310f6772c5fe7b90d0828eba28bde13bb5
VictoriaVAlex <aleksuk@bk.ru>
2024-08-11 18:03:27 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+my_string = input("Какое сейчас время года?")
+print(len(my_string))
+my_string = ("Какое сейчас время года?")
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print(my_string[0])
+print(my_string[-1])
+
