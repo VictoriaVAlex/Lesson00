577b89e2fbd466f9caa90374721ab98411fabad4
VictoriaVAlex <aleksuk@bk.ru>
2024-08-11 19:58:17 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = [1,2,3], 2, "False"
+print(immutable_var)
+#immutable_var[2] = 3 #кортеж относится к типу неизменяемых данных
+mutable_list = [1,2,3,2,"False"]
+mutable_list.append("New_Element")
+print(mutable_list)
